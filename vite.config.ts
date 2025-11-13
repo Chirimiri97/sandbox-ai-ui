e419ae7629f7b29c95f20a266712c2eafcfc850b
Chirag Jain <jainch1997@gmail.com>
2025-11-14 03:58:24 +0530

    Removed prettier and formatted all files using "language server".

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,14 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	server: {
-		port: 3000,
-		strictPort: true,
-	},
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
 });
