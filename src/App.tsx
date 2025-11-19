f0bc5b79fa717de00214978c96ee022832ab592f
Chirag Jain <jainch1997@gmail.com>
2025-11-19 14:51:37 +0530

    Navbar changes (Stash Pop).

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
 
+// ShadCN Components.
+import { Button } from "./components/ui/button";
+
+// Components.
+import Navbar from "./components/navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen p-8 pt-24">
+        <div className="">
+          <h2 className="text-2xl font-bold font-josefin">Hello Chirag!</h2>
+          {/*<h3 className="text-2xl font-bold font-josefin">
+            Welcome to SandboxGPT,
+          </h3>*/}
+          <p className="text-sm font-mono">How can I help you today?</p>
+
+          <Button variant={"outline"} className="font-mono font-medium">
+            Click Me
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="fixed top-0.5 left-0.5 right-0.5 h-16 min-h-1.5 bg-foreground text-background flex items-center p-8">
+      <a className="font-josefin font-extrabold">SandboxGPT</a>
+    </nav>
+  );
+}
+
+export default Navbar;
